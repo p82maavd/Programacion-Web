7d74e7a65f3bc3fb1c4bc9cd7fcf3f8bf0c1372e
Damián Martínez <55441341+p82maavd@users.noreply.github.com>
2020-09-29 17:02:30 +0200

    Update GestorContactos.java

--- GestorContactos.java
+++ GestorContactos.java
@@ -22,4 +22,126 @@ public class GestorContactos {
 		this.listaContactos = new ArrayList<Contacto>();
 		
   }
+	
+	public void cargarDatos() throws FileNotFoundException, IOException, ClassNotFoundException {
+		ObjectInputStream file = new ObjectInputStream(new FileInputStream("fich.dat"));
+		ArrayList<String> aux=new ArrayList<String>();
+		Date auxi= new Date();
+		Contacto clase= new Contacto("Auxiliar","Auxiliar",auxi, "auxiliar@hotmail.es",aux);
+		
+		while(clase!=null) {
+			 try {
+				 clase = (Contacto) file.readObject(); 
+		         } catch (EOFException e) {
+		            System.out.println("");
+		            System.out.println("End of file reached");
+		            break;
+		         } 
+			
+			this.listaContactos.add(clase);        	
+		}
+		file.close();
+		 
+		
+	}
+	
+	public void guardarDatos() throws FileNotFoundException, IOException {
+		
+		ObjectOutputStream file = new ObjectOutputStream(new FileOutputStream( "patatilla.dat" ));
+		Date auxi= new Date();
+        ArrayList<String> aux=new ArrayList<String>();
+		Contacto auxc=new Contacto("Auxiliar","Auxiliar",auxi, "auxiliar@hotmail.es",aux);
+		
+		for(int i=0; i<this.listaContactos.size();i++) {
+			
+			auxc=this.listaContactos.get(i);
+	        file.writeObject(auxc);
+			
+		}
+        
+        file.close();
+	}
+	
+	public void imprimirContacto(Contacto e) {
+		String cadena=new String();
+		System.out.println("Nombre: "+e.getNombre()+" Apellidos: "+ e.getApellidos()+" Email: "+e.getEmail()+" Fecha de Nacimiento: "+ e.getFechanacimiento());
+		for(int i=0; i<e.getIntereses().size();i++) {
+			cadena=e.getIntereses().get(i);
+			System.out.println(cadena);
+		}
+	}
+	
+	public void imprimirDatos() throws FileNotFoundException, IOException, ClassNotFoundException {
+		
+		for(int i=0; i<this.listaContactos.size();i++) {
+			
+			imprimirContacto(this.listaContactos.get(i));
+		}
+	
+	}
+	
+	public void darAlta() throws IOException {
+		
+			Scanner sc = new Scanner(System.in);
+			String nuevonombre;
+			System.out.print("Introduzca el nuevo nombre: ");
+			nuevonombre = sc.nextLine();
+			
+			
+			String nuevoapellido;
+			System.out.print("Introduzca el nuevo apellido: ");
+			nuevoapellido = sc.nextLine();
+			
+			
+			String nuevoemail;
+			System.out.print("Introduzca el nuevo email: ");
+			nuevoemail = sc.nextLine();
+			
+			
+			String nuevafecha="01/01/1970";
+			System.out.print("Introduzca la nueva fecha de nacimiento(dd/mm/yyyy): ");
+			nuevafecha = sc.nextLine();
+			
+			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+			 
+			Date dnuevafecha = null;
+			try {
+				dnuevafecha = formatter.parse(nuevafecha);
+			} catch (ParseException e1) {
+				
+				e1.printStackTrace();
+			}
+			
+			
+			ArrayList<String> intereses=new ArrayList<String>();
+			Integer neweleccion=0;
+			String newinteres=new String();
+			Boolean condicion=true;
+			Scanner alt = new Scanner(System.in);
+			
+			while(condicion) {
+				
+				System.out.println("Introduzca un nuevo interes: ");
+				
+				newinteres= alt.nextLine();
+				
+				intereses.add(newinteres);
+				
+				
+				System.out.println("Desea añadir mas intereses: 1. Si 2. No");
+				neweleccion=alt.nextInt();
+				alt.nextLine();
+				if(neweleccion!=1) {
+					condicion=false;
+				}
+				
+			
+			}
+			
+			Contacto e=new Contacto(nuevonombre,nuevoapellido,dnuevafecha,nuevoemail,intereses);
+			
+			this.listaContactos.add(e);
+			
+			this.guardarDatos();
+
 }
