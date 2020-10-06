f97a19cf68af2f1d4ce8293b565031d67493c1c6
Damián Martínez <55441341+p82maavd@users.noreply.github.com>
2020-10-06 09:55:22 +0200

    Update GestorContactos.java

--- Practica 1/Ejercicio 1/GestorContactos.java
+++ Practica 1/Ejercicio 1/GestorContactos.java
@@ -1,115 +1,72 @@
+/**
+ * 
+ * @author Damian Martinez
+ * @author Daniel Ortega
+ */
 
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.EOFException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
-//Cambiar dependiendo de donde lo tengas guardado
+
 import practica1entrega.Contacto;
 import practica1entrega.Contacto.Intereses;
 
+
 public class GestorContactos {
+	
 	private static GestorContactos instance =null;
 	
 	private ArrayList <Contacto> listaContactos;
 
-	//Singleton
-	
+	/**
+	 * Este método se encarga de crear una instancia en el caso de que no haya una ya creada. Patron de diseño Singleton
+	 * @return Instancia única de GestorContactos.
+	*/
 	public static GestorContactos getInstance() {
+		
 		if(instance==null) {
 			instance=new GestorContactos();
 		}
 		return instance;
 	}
-	//Final Singleton
 	
-	public GestorContactos() {
+	/**
+	 * Constructor de la clase GestorContactos.
+	*/
+	private GestorContactos() {
 		
 		this.listaContactos = new ArrayList<Contacto>();
 		
-  }
-	
-	public void cargarDatos() throws FileNotFoundException, IOException, ClassNotFoundException {
-		ObjectInputStream file = new ObjectInputStream(new FileInputStream("fich.dat"));
-		ArrayList<String> aux=new ArrayList<String>();
-		Date auxi= new Date();
-		Contacto clase= new Contacto("Auxiliar","Auxiliar",auxi, "auxiliar@hotmail.es",aux);
-		
-		while(clase!=null) {
-			 try {
-				 clase = (Contacto) file.readObject(); 
-		         } catch (EOFException e) {
-		            System.out.println("");
-		            System.out.println("End of file reached");
-		            break;
-		         } 
-			
-			this.listaContactos.add(clase);        	
-		}
-		file.close();
-		 
-		
 	}
 	
-	public void guardarDatos() throws FileNotFoundException, IOException {
-		
-		ObjectOutputStream file = new ObjectOutputStream(new FileOutputStream( "fich.dat" ));
-		Date auxi= new Date();
-        ArrayList<Intereses> aux=new ArrayList<Intereses>();
-		Contacto auxc=new Contacto("Auxiliar","Auxiliar",auxi, "auxiliar@hotmail.es",aux);
-		
-		for(int i=0; i<this.listaContactos.size();i++) {
-			
-			auxc=this.listaContactos.get(i);
-	        file.writeObject(auxc);
-			
-		}
-        
-        	file.close();
-	}
-	
-	public void consultarContacto(Contacto e) {
-		String cadena=new String();
-		System.out.println("Nombre: "+e.getNombre()+" Apellidos: "+ e.getApellidos()+" Email: "+e.getEmail()+" Fecha de Nacimiento: "+ e.getFechanacimiento());
-		for(int i=0; i<e.getIntereses().size();i++) {
-			cadena=e.getIntereses().get(i).getInteres();
-			System.out.println(cadena);
-		}
-	}
-	
-	public void imprimirDatos() throws FileNotFoundException, IOException, ClassNotFoundException {
-		
-		for(int i=0; i<this.listaContactos.size();i++) {
-			
-			consultarContacto(this.listaContactos.get(i));
-		}
-	
-	}
-	
+	/**
+	 * Este método se encarga de dar de alta a un contacto.
+	*/
 	public void darAlta() throws IOException {
 		
 			Scanner sc = new Scanner(System.in);
+		
 			String nuevonombre;
 			System.out.print("Introduzca el nuevo nombre: ");
 			nuevonombre = sc.nextLine();
-			
 			
 			String nuevoapellido;
 			System.out.print("Introduzca el nuevo apellido: ");
 			nuevoapellido = sc.nextLine();
 			
-			
 			Boolean email= true;
 			String nuevoemail=new String();
-			
+			//Comprueba que el email que se introduzca no este repetido.
 			while(email) {
 				
 				System.out.print("Introduzca el email: ");
@@ -134,7 +91,6 @@ public class GestorContactos {
 				
 			}
 			
-			
 			String nuevafecha="01/01/1970";
 			System.out.print("Introduzca la nueva fecha de nacimiento(dd/mm/yyyy): ");
 			nuevafecha = sc.nextLine();
@@ -145,7 +101,6 @@ public class GestorContactos {
 			try {
 				dnuevafecha = formatter.parse(nuevafecha);
 			} catch (ParseException e1) {
-				
 				e1.printStackTrace();
 			}
 			
@@ -165,8 +120,7 @@ public class GestorContactos {
 				}
 				
 				newinteres= alt.nextLine();
-				
-				int foo;
+				//Actualizarlo como en el de actualizarContacto.
 				try {
 				   foo = Integer.parseInt(newinteres);
 				}
@@ -180,7 +134,7 @@ public class GestorContactos {
 						intereses.add(myVar);
 					}
 				}
-
+				
 				System.out.println("Desea añadir mas intereses: 1. Si 2. No");
 				neweleccion=alt.nextInt();
 				alt.nextLine();
@@ -188,22 +142,35 @@ public class GestorContactos {
 					condicion=false;
 				}
 				
+			
 			}
+			
+			//NO CERRAR SCANNERS, NO FUNCIONA EL MAIN.
+			//alt.close();
+			//sc.close();
 			
 			Contacto e=new Contacto(nuevonombre,nuevoapellido,dnuevafecha,nuevoemail,intereses);
 			
 			this.listaContactos.add(e);
 			
 			this.guardarDatos();
+		
 	}
 	
-	public void darBaja(int index) {
-		//Esto esta sin implementar. Solo para limpiar el archivo.
+	/**
+	 * Este método se encarga de dar de baja a un contacto.
+	 * @param Contacto que desea dar de baja
+	*/
+	public void darBaja(int index) throws FileNotFoundException, IOException {
+		//Implementar cuando buscarContacto().
 		this.listaContactos.remove(index);
 		this.guardarDatos();
 		
 	}
 	
+	/**
+	 * Este método se encarga de actualizar un contacto.
+	*/
 	public void actualizarContacto() {
 		
 		//Esta para poner de parametro la funcion buscarContacto(), de mientras coge el primero.
@@ -282,13 +249,13 @@ public class GestorContactos {
 			else {
 				
 				System.out.println("Seleccione un nuevo interes: ");
-				String newinteres=new String();
+				int newinteres=0;
 				for (Intereses myVar : Intereses.values()) {
 					System.out.println(myVar.getId()+" "+myVar.getInteres());
 				}
-				//Probar, posible fallo por scanner
-				newinteres= sc.nextLine();
 				
+				newinteres= sc.nextInt();
+				/*
 				int foo;
 				try {
 				   foo = Integer.parseInt(newinteres);
@@ -297,9 +264,10 @@ public class GestorContactos {
 				{
 				   foo = 0;
 				}
+				*/
 				
 				for (Intereses myVar : Intereses.values()) {
-					if(foo==myVar.getId()) {
+					if(newinteres==myVar.getId()) {
 						intereses.add(myVar);
 					}
 				}
@@ -307,9 +275,80 @@ public class GestorContactos {
 			
 			
 		}
-		this.guardarDatos();
+		
 		//sc.close();
 		
+	}
+	
+	/**
+	 * Este método se encarga de leer los contactos del fichero.
+	 * @return Instancia única de GestorContactos.
+	*/
+	public void cargarDatos() throws FileNotFoundException, IOException, ClassNotFoundException {
+		ObjectInputStream file = new ObjectInputStream(new FileInputStream("fich.dat"));
+		ArrayList<Intereses> aux=new ArrayList<Intereses>();
+		Date auxi= new Date();
+		Contacto clase= new Contacto("Auxiliar","Auxiliar",auxi, "auxiliar@hotmail.es",aux);
+		
+		while(clase!=null) {
+			 try {
+				 clase = (Contacto) file.readObject(); 
+		         } catch (EOFException e) {
+		            System.out.println("");
+		            System.out.println("Contactos cargados correctamente");
+		            break;
+		         } 
+			
+			this.listaContactos.add(clase);        	
+		}
+		file.close();
+		 
+		
+	}
+	
+	/**
+	 * Este método se encarga de guardar los contactos en el fichero de datos.
+	*/
+	public void guardarDatos() throws FileNotFoundException, IOException {
+		
+		ObjectOutputStream file = new ObjectOutputStream(new FileOutputStream( "fich.dat" ));
+		Date auxi= new Date();
+        ArrayList<Intereses> aux=new ArrayList<Intereses>();
+		Contacto auxc=new Contacto("Auxiliar","Auxiliar",auxi, "auxiliar@hotmail.es",aux);
+		
+		for(int i=0; i<this.listaContactos.size();i++) {
+			
+			auxc=this.listaContactos.get(i);
+	        file.writeObject(auxc);
+			
+		}
+        
+        file.close();
+	}
+	
+	/**
+	 * Este método se encarga de imprimir los datos de todos los contactos.
+	*/
+	public void imprimirDatos() throws FileNotFoundException, IOException, ClassNotFoundException {
+		
+		for(int i=0; i<this.listaContactos.size();i++) {
+			
+			consultarContacto(this.listaContactos.get(i));
+		}
+	
+	}
+	
+	/**
+	 * Este método se encarga de imprimir los datos de un contacto.
+	*/
+	public void consultarContacto(Contacto e) {
+		String cadena=new String();
+		System.out.println("Nombre: "+e.getNombre()+" Apellidos: "+ e.getApellidos()+" Email: "+e.getEmail()+" Fecha de Nacimiento: "+ e.getFechanacimiento());
+		for(int i=0; i<e.getIntereses().size();i++) {
+			cadena=e.getIntereses().get(i).getInteres();
+			
+			System.out.println(cadena);
+		}
 	}
 
 }
