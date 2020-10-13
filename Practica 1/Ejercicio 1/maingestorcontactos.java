16e9d02a048477abc7f653a94ce62678912caa73
Damián Martínez <55441341+p82maavd@users.noreply.github.com>
2020-10-13 21:22:48 +0200

    Update maingestorcontactos.java

--- Practica 1/Ejercicio 1/maingestorcontactos.java
+++ Practica 1/Ejercicio 1/maingestorcontactos.java
@@ -9,12 +9,7 @@ public class maingestorcontactos {
 	
 		Scanner sc = new Scanner(System.in);
 		Boolean condicion=true;
-		try {
-		gestor.cargarDatos();
-		}catch(FileNotFoundException e) {
-			ObjectOutputStream file = new ObjectOutputStream(new FileOutputStream("fich.dat"));
-	        file.close();
-		}
+		
 		int a= 0;
 		
 		while(condicion) {
